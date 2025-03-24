41147dca3026b9d1113bc3e30b90fb1447a78e47
TE109 <1991tmce@gmail.com>
2025-03-24 11:57:32 -0400

    Initial Commit

---
+++ A2_iOS_-Tomer_101400506/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_iOS_-Tomer_101400506
+//
+//  Created by user271259 on 3/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ A2_iOS_-Tomer_101400506Tests/A2_iOS__Tomer_101400506Tests.swift
@@ -0,0 +1,17 @@
+//
+//  A2_iOS__Tomer_101400506Tests.swift
+//  A2_iOS_-Tomer_101400506Tests
+//
+//  Created by user271259 on 3/24/25.
+//
+
+import Testing
+@testable import A2_iOS__Tomer_101400506
+
+struct A2_iOS__Tomer_101400506Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
