0ac03d002c0309506dcf990d3e1299a062efe7be
TE109 <1991tmce@gmail.com>
2025-03-25 10:31:41 -0400

    Adding forward and backward view and formating text label

--- A2_iOS_-Tomer_101400506/ViewController.swift
+++ A2_iOS_-Tomer_101400506/ViewController.swift
@@ -8,93 +8,102 @@
 import UIKit
 import CoreData
 
-// https://www.youtube.com/watch?v=O7u9nYWjvKk&t=2344s
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet var tableView: UITableView!
+    
+    var index = 0;
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    var items: [Products]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        fetchProducts()
+    }
+
+    func fetchProducts() {
+        do {
+            self.items = try context.fetch(Products.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            print("Failed to fetch products: \(error)")
+        }
+    }
+
+    
+    @IBAction func forwardPress(_ sender: Any) {
+        if index < (items?.count ?? 0) - 1 {
+            index += 1
+            fetchProducts()
+        }
+    }
+    
+    @IBAction func backwardPress(_ sender: Any) {
+        if index > 0 {
+            index -= 1
+            fetchProducts()
+        }
+    }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        //return 1
-        return items?.count ?? 1
+        return 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
-        let item = self.items?[indexPath.row]
-        cell.textLabel?.text = item?.name
-        //cell.textLabel?.text = "Placeholder"
-        return cell
-    }
-    
-    @IBOutlet var tableView: UITableView!
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext;
-    
-    var items:[Products]?
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view
-        
-        tableView.dataSource = self;
-        tableView.delegate = self;
-        
-        fetchProducts()
-    }
-    
-    
-    
-    func fetchProducts() {
-        do {
-            self.items = try context.fetch(Products.fetchRequest())
-            
-            tableView.reloadData()
-        } catch {
-            
+        if let firstItem = items?[index] {
+            cell.textLabel?.numberOfLines = 0
+            cell.textLabel?.text = """
+            Name: \(firstItem.name ?? "")
+            Price: $\(firstItem.price)
+            Description: \(firstItem.desc ?? "")
+            Provider: \(firstItem.provider ?? "")
+            """
+        } else {
+            cell.textLabel?.text = "No Data Available"
         }
         
+        return cell
     }
-    
+
     @IBAction func addTapped(_ sender: Any) {
         let alert = UIAlertController(title: "Add Product", message: "Enter the details for the new product", preferredStyle: .alert)
         
-        alert.addTextField { textField in
-            textField.placeholder = "Name"
-        }
-        alert.addTextField { textField in
-            textField.placeholder = "Price"
-            textField.keyboardType = .decimalPad
-        }
-        alert.addTextField { textField in
-            textField.placeholder = "Description"
-        }
-        alert.addTextField { textField in
-            textField.placeholder = "Provider"
-        }
+        alert.addTextField { textField in textField.placeholder = "Name" }
+        alert.addTextField { textField in textField.placeholder = "Price"; textField.keyboardType = .decimalPad }
+        alert.addTextField { textField in textField.placeholder = "Description" }
+        alert.addTextField { textField in textField.placeholder = "Provider" }
+
         let submitButton = UIAlertAction(title: "Add", style: .default) { _ in
             let nameField = alert.textFields![0].text ?? ""
             let priceField = alert.textFields![1].text ?? "0"
             let descField = alert.textFields![2].text ?? ""
-            let ProviderField = alert.textFields![3].text ?? ""
-            
-            
+            let providerField = alert.textFields![3].text ?? ""
+
             let newProduct = Products(context: self.context)
             newProduct.name = nameField
             newProduct.price = Double(priceField) ?? 0.0
             newProduct.desc = descField
-            newProduct.provider = ProviderField
-            
+            newProduct.provider = providerField
+
             do {
                 try self.context.save()
                 self.fetchProducts()
             } catch {
+                print("Failed to save product: \(error)")
             }
         }
         
-        let cancelButton = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-        
         alert.addAction(submitButton)
-        alert.addAction(cancelButton)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         
         self.present(alert, animated: true, completion: nil)
     }
-    
 }
